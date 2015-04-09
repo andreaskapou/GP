568ffa3b2b9097a32f495a6ff083f17bf7726baf
andreaskapou <kapouranis.andreas@gmail.com>
2015-04-09 17:08:03 +0100

    Check if the parameter is of Object List

--- binCumGauss.R
+++ binCumGauss.R
@@ -1,4 +1,4 @@
-binCumGauss <- function(f, m, k, pred){
+binCumGauss <- function(f, y, pred){
   ##=====================================================================
   # binCumGauss - Binomial Distributed Cumulative Gaussian likelihood   #
   # function. The expression for log likelihood is                      #
@@ -7,12 +7,12 @@ binCumGauss <- function(f, m, k, pred){
   # Two modes are provided, for computing likelihoods and derivatives   #
   # and for computing the approximate predictive probability.           #
   #                                                                     #
-  # Usage: Phi <- cumGaus(f, m, k)                                      #
-  #    or: Phi <- cumGaus(f, m, k, pred)                                #
+  # Usage: Phi <- cumGaus(f, y)                                         #
+  #    or: Phi <- cumGaus(f, y, pred)                                   #
   # where in Mode 1:                                                    #
   #     f     is a vector of inputs where the CDF needs to be evaluated #
-  #     m     is a vector of the total number of trials                 #
-  #     k     is a vector of the corresponding successes on each trial  #
+  #     y$m   is a vector of the total number of trials                 #
+  #     y$k   is a vector of the corresponding successes on each trial  #
   #                                                                     #
   #     lp    is log p(y|f) log likelihood                              #
   #     d1lp  is the 1st derivative                                     #
@@ -26,23 +26,29 @@ binCumGauss <- function(f, m, k, pred){
   #     pi.pred is the approximate predictive probability               #
   #                                                                     #
   ##=====================================================================
+  
+  if (!is.list(y) | length(y) != 2){
+    stop("y should be an Object of type List and contain two entries")
+  }
+  m <- y$m  # Number of total trials
+  k <- y$k  # Number of successes in the corresponding m trials
+  
   if (missing(pred)){
     Phi   <- pnorm(f) + 1e-10   # Cumulative Density Function of N(0,1)
     N     <- dnorm(f)           # Density for function f under N(0,1)
     # Compute -> log(choose(m, k)) + k*log(Phi) + (m-k)*log(1-Phi)
-    llik  <- dbinom(k, m, Phi, log=TRUE)
+    llik  <- dbinom(x=k, size=m, prob=Phi, log=TRUE)
     lp    <- sum(llik)          # log p(y|f) log likelihood
     
-    d1lp  <- k*N/Phi - (m-k)*N/(1-Phi)    # 1st derivative
+    # Compute 1st, 2nd and 3rd derivatives wrt to latent function f
+    d1lp  <- k*N/Phi - (m-k)*N/(1-Phi)
     d2lp  <- (-k*(N*(f*Phi+N)/Phi^2)) - (m-k)*(N*(N-f*(1-Phi))/(1-Phi)^2)
-    d3lp  <- (-k*(N*(-2*(N^2)-3*N*x*Phi-(Phi^2)*((x^2)-1)))/(Phi^3)) - 
-                (m-k)*(N*(N*(2*N-3*x+3*x*Phi)+(x^2)*((Phi^2)-2*Phi+1)-
+    d3lp  <- (-k*(N*(-2*(N^2)-3*N*f*Phi-(Phi^2)*((f^2)-1)))/(Phi^3)) - 
+                (m-k)*(N*(N*(2*N-3*f+3*f*Phi)+(f^2)*((Phi^2)-2*Phi+1)-
                                             (Phi^2)+2*Phi-1)/((1-Phi)^3))
     return(list(lp=lp, d1lp=d1lp, d2lp=d2lp, d3lp=d3lp))
-  }else{
-    pi.pred <- pnorm(f/sqrt(1 + y)) # Approximate predictive probability for 
-    # the probit likelihood i.e.
-    # p(y*=1|D,t,x*) = N.CDF((m*/sqrt(1+s2*)))
+  }else{ # Not implemented yet
+    pi.pred <- pnorm(f/sqrt(1 + 1)) # y
     return(pi.pred)
   }
 }
